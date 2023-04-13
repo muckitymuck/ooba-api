6a6a5561042f5c9e9171e4880bf40c257d03f939
disarmyouwitha <disarmyouwitha@gmail.com>
2023-04-12 19:27:25 -0500

    adding new params from new ooba commits

--- main.py
+++ main.py
@@ -83,9 +83,9 @@ class GenerateRequest(BaseModel):
     return_prompt: Optional[bool] = False
     #stop: Optional[str] = None
     add_bos_token: Optional[bool] = True
-    truncation_length: 2048,
-    custom_stopping_strings: Optional[str] = '',
-    ban_eos_token: Optional[bool] =False,
+    truncation_length: 2048
+    custom_stopping_strings: Optional[str] = ''
+    ban_eos_token: Optional[bool] =False
 
 @app.get("/")
 def hellow_world(q: Union[str, None] = None):
