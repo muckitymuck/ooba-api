1415019bc8936e9d7c5bf75cfc64e7a84d9ca366
disarmyouwitha <disarmyouwitha@gmail.com>
2023-04-15 23:15:27 -0500

    test on this side

--- main.py
+++ main.py
@@ -212,11 +212,38 @@ def progress(req: ProgressRequest):
     return ProgressResponse(active=active, queued=queued, completed=completed, progress="{0}".format(1), textinfo="currently processing")
 
 
+@app.post("/chat/generate")
+async def fw(req: GenerateRequest):
+
+    # move everything into an async function..
+    # return it as a return stream?
+    
+    async def gen():
+        req_params = {
+            'prompt': req.prompt,
+            'max_new_tokens': req.max_new_tokens,
+            'temperature': req.temperature,
+            'streaming': req.streaming
+        }
+        print(req.prompt)
+
+        r = requests.post("http://localhost:7861/generate", data=json.dumps(req_params), stream=True)
+
+        # try to re-yield this and stream it back:
+        if r.status_code==200:
+            for chunk in r.iter_content(chunk_size=64):
+                if chunk:
+                    print(chunk.decode("utf-8"), flush=True)
+                    yield chunk.decode("utf-8")
+
+    return StreamingResponse(gen())
+
+
 # in generate strip to the last . rather than ending in the middle of a sentence. (?)
 @app.post("/generate")
 async def stream_data(req: GenerateRequest):
     print(req.prompt)
-    print("streaming?: {0}".format(req.streaming))
+    #print("streaming?: {0}".format(req.streaming))
 
     prompt = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 ### Instruction:
@@ -253,10 +280,8 @@ async def stream_data(req: GenerateRequest):
 
     # Hooking no_stream arg so that we can set streaming value from here:
     if req.streaming:
-        print("strem")
         shared.args.no_stream = False
     else:
-        print("no_strem")
         shared.args.no_stream = True
     
     # start generating response:
--- send_post.py
+++ send_post.py
@@ -10,10 +10,17 @@ if not sys.argv[1]:
 data = {
     "prompt": sys.argv[1],
     "temperature": 0.7,
+    #"streaming": False
 }
 
+r = requests.post("http://wintermute:7861/generate", data=json.dumps(data), stream=True)
+#r = requests.post("http://localhost:7861/generate", data=json.dumps(data), stream=True)
 #r = requests.post("http://wintermute:7861/queue", data=json.dumps(data))
-r = requests.post("http://localhost:7861/queue", data=json.dumps(data))
+#r = requests.post("http://localhost:7861/queue", data=json.dumps(data))
 print(r.status_code)
+
 if r.status_code==200:
-    print(r.json())
+    for chunk in r.iter_content(chunk_size=64):
+        if chunk:
+            # Do something with the chunk of data
+            print(chunk.decode("utf-8"), flush=True)
