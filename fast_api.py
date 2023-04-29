1b5535124afda4abf980f90eb6b360bdbf627e1b
disarmyouwitha <disarmyouwitha@gmail.com>
2023-04-29 11:54:13 -0500

    db logging for evals

--- fast_api.py
+++ fast_api.py
@@ -212,12 +212,12 @@ async def stream_data(req: GenerateRequest):
                 # tokens/sec:
                 t1 = time.time()
                 original_tokens = len(encode(req.prompt)[0])
-                new_tokens = len(encode(_full_answer)[0]) - original_tokens
+                new_tokens = len(encode(_full_answer)[0])
                 print(req.prompt)
                 print(original_tokens)
                 print("_____")
                 print(_full_answer)
-                print(new_tokens)
+                print(len(encode(_full_answer)[0]))
                 print(f'init: Output generated in {(t1-t0):.2f} seconds ({new_tokens/(t1-t0):.2f} tokens/s, {new_tokens} tokens, context {original_tokens})')
 
                 # Execute an insert query
